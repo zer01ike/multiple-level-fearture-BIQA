385a7af8f564f025d01d73acfd277337a46736ba
zer01ike <wangkai_like@163.com>
2018-11-20 20:50:28 +0800

    add testfiles

---
+++ test/resent_reload.py
@@ -0,0 +1,24 @@
+from tensorflow.contrib.slim.nets import resnet_v1
+from keras_preprocessing import image
+import numpy as np
+import tensorflow as tf
+import tensorflow.contrib.slim as slim
+
+batch_size = 1
+height = 224
+width = 224
+channels = 3
+path_to_ckpt = 'K:\\resnet\\resnet_v1_50.ckpt'
+
+inputs  = tf.placeholder(tf.float32,shape=[batch_size,height,width,channels])
+with slim.arg_scope(resnet_v1.resnet_arg_scope()):
+    net,end_points = resnet_v1.resnet_v1_50(inputs,is_training=False)
+
+saver = tf.train.Saver()
+X_test = np.ones((1,224,224,3))
+with tf.Session() as sess:
+    saver.restore(sess,path_to_ckpt)
+    spacific_tensor = sess.graph.get_tensor_by_name('resnet_v1_50/conv1:0')
+    #img = image.load_img('',target_size=(224,224))
+    features = sess.run(spacific_tensor,{'Placeholder:0':X_test})
+    print(features.shape)
