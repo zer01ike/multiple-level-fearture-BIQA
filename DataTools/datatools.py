759eae99e42f34991d602cf975fd2b5bd3e75eac
zer0like <wangkai_like@163.com>
2018-12-03 14:42:19 +0800

    finish the datatools.py

--- DataTools/datatools.py
+++ DataTools/datatools.py
@@ -21,9 +21,113 @@ class image():
         return self.ref+slash+self.distoration+slash+self.name+slash+str(patch_number)+"."+store_fmt
 
 
-def generateMeanImage():
-    pass
+def preporcessing(data_dir,summary_file,meanpatch_file,train_percent,test_percent):
 
+
+    X_train ,Y_train,X_test,Y_test = generateTrainTest(data_dir=data_dir,
+                                                       summary_file = summary_file,
+                                                       train_percent= train_percent,
+                                                       test_percent = test_percent)
+
+
+    meanPatch = readImage(meanpatch_file)
+    meanPatch = np.array(meanPatch)
+
+    X_train,X_test = minusMeanPtacth(X_train,X_test,meanPatch)
+
+    return X_train, Y_train,X_test,Y_test
+
+def generateTrainTest(data_dir,summary_file,train_percent,test_percent):
+    X=[]
+    Y=[]
+    X_train = []
+    Y_train = []
+    X_test = []
+    Y_test = []
+
+    # need to make sure the ref category is ok
+    # so split method is base on the percentge of ref images
+
+    category = []
+
+    if(train_percent + test_percent != 1) :
+        print("Error: The train and test percent is not equal to 100%")
+        exit(code= 5)
+
+
+
+    #read all images from the data_dir + summary_file
+    with open(summary_file,'r') as summary:
+        lines = summary.readlines()
+        for line in lines:
+            line = line.replace("\n","")
+            dmos, name = line.split(" ")
+            class_name = name.split("_")[0]
+            X.append(name)
+            Y.append(dmos)
+            category.append(class_name)
+    category = set(category)
+
+    category = list(category)
+    #split the category
+    indices = int(len(category) * train_percent)
+    train_category = category[:indices]
+    test_category = category[indices:]
+    print("Category Contain:".ljust(25)+str(category))
+    print("Train Category Contain:".ljust(25)+str(train_category))
+    print("Test Category Contain:".ljust(25)+str(test_category))
+
+    for unsplit_x,unsplit_y in zip(X,Y):
+        isTrain = True if unsplit_x.split("_")[0] in train_category else False
+        isTest = True if unsplit_x.split("_")[0] in test_category else False
+        if isTrain :
+            X_train.append(readImage(str(data_dir+unsplit_x)))
+            Y_train.append(unsplit_y)
+        elif isTest :
+            X_test.append(readImage(str(data_dir+unsplit_x)))
+            Y_test.append(unsplit_y)
+
+    X_train = np.array(X_train)
+    Y_train = np.array(Y_train)
+    X_test = np.array(X_test)
+    Y_test = np.array(Y_test)
+
+    print("X_train_size:".ljust(25)+str(len(X_train)) +"  "+ str(X_train.shape) )
+    print("Y_train_size:".ljust(25) + str(len(Y_train))+"  "+ str(Y_train.shape))
+    print("X_test_size:".ljust(25) + str(len(X_test))+"  "+ str(X_test.shape))
+    print("Y_test_size:".ljust(25) + str(len(Y_test))+"  "+ str(Y_test.shape))
+    return X_train,Y_train,X_test,Y_test
+
+def minusMeanPtacth(X_train,X_test,meanPatch):
+    X_train = X_train - meanPatch
+    X_test = X_test - meanPatch
+    return X_train, X_test
+
+
+def generateMeanpatch(patch_dir):
+    # read all Patch
+    import os
+    Patchs = []
+    for root, dirs,files in os.walk(patch_dir):
+        for file in files:
+            Patchs.append(os.path.join(root,file))
+
+    sum_mean = np.zeros([3], dtype=np.float64)
+    index = 0
+    for patch in Patchs:
+        patch_content = readImage(patch)
+        sum_mean += np.mean(patch_content, axis=(0, 1))
+        if (index %100 == 0):
+            print("Proced:".ljust(10)+str(index+1).rjust(8)+"/"+str(len(Patchs)))
+        index+=1
+
+    total = np.array([len(Patchs),len(Patchs),len(Patchs)])
+
+    all_mean = np.true_divide(sum_mean,total)
+
+    average_patch = np.tile(all_mean, (224, 224, 1))
+
+    return average_patch
 
 def getImageInfo(data_dir,IQA_name):
     '''
@@ -37,6 +141,7 @@ def getImageInfo(data_dir,IQA_name):
     with open(parentdir + IQA_name) as pathfile:
         lines_pathfile = pathfile.readlines()
 
+        # this is the pattern of the file in IQA_name.txt
         for eachline in lines_pathfile:
             single_imagepath = eachline.split(" ")[3]
             single_ref = eachline.split(" ")[2].split("/")[1].split('.')[0]
@@ -50,25 +155,27 @@ def getImageInfo(data_dir,IQA_name):
     return imagesinfo
 
 
-def getImagePatch():
-    pass
+def generateInfoFile(data_dir,IQA_name,file_name,randomcut_number):
+    imagesinfo = getImageInfo(data_dir,IQA_name)
+    index = 0
+    with open(data_dir+file_name,'w') as saved:
+        for image in imagesinfo:
+            for i in range(0,randomcut_number):
+                croped_image_name = image.storeName(index % randomcut_number, 'png')
+                croped_image_dmos = image.demos_normal
+                index +=1
+                saved.write(str(croped_image_dmos)+" "+croped_image_name+"\n")
 
-def readImage(imagespath):
-    import cv2
 
-    pass
+def readImage(patch):
+    image = cv2.imread(patch)
 
-def readLabel():
-    pass
+    if image is None:
+        print("error Read:".ljust(10) + patch)
+        exit(15)
 
-def checkConsistency():
-    pass
+    return image
 
-def saveImage():
-    pass
-
-def saveLabel():
-    pass
 
 def cropImagerandom_tf(imagesinfo,num):
     tf.set_random_seed(66)
@@ -104,9 +211,6 @@ def cropImagerandom_tf(imagesinfo,num):
             index +=1
 
 
-def cropImagerandom():
-    pass
-
 ###################################test case ##############33
 
 def test_getImageInfo():
@@ -125,16 +229,29 @@ def test_cropImage():
     print("ImageInfo Readed!!!")
     cropImagerandom_tf(imageinfo[0:5],50)
 
+def test_generateMeanpatch():
+    data_dir = "/home/wangkai/Paper_MultiFeature_Data/databaserelease2/Patched_data/"
+    average_image = generateMeanpatch(data_dir)
+    cv2.imwrite("average_mean.png",average_image)
 
-def cropImage():
+def test_generateinfofile():
     data_dir = "/home/wangkai/Paper_MultiFeature_Data/databaserelease2/"
     IQA_name = "LIVE_IQA.txt"
-    imageinfo = getImageInfo(data_dir, IQA_name)
-    cropImagerandom_tf(imageinfo,50)
+    infofile = "croped_info.txt"
+    generateInfoFile(data_dir,IQA_name,infofile,50)
 
+def test_generateTrainTest():
+    data_dir = "/home/wangkai/Paper_MultiFeature_Data/databaserelease2/Patched_data/"
+    summary_file = "/home/wangkai/Paper_MultiFeature_Data/databaserelease2/croped_info.txt"
 
-
-
+    generateTrainTest(data_dir,summary_file,0.8,0.2)
+def test_imageread():
+    a = readImage("/home/wangkai/Paper_MultiFeature_Data/databaserelease2/Patched_data/bikes_jp2k_img15_0.png")
+    b = readImage("/home/wangkai/Paper_MultiFeature_Data/databaserelease2/Patched_data/bikes_jp2k_img15_0.png")
 
 if __name__ == '__main__':
-    cropImage()
+    #cropImage()
+    #test_generateinfofile()
+    #test_generateTrainTest()
+    #test_imageread()
+    pass
